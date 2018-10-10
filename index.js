34cbd2b1057687b6628a51168459cb2ed510752c
Glitch (monitori) <none>
2018-10-10 02:03:51 +0000

    Glitch hosting

--- index.js
+++ index.js
@@ -249,5 +249,17 @@ bot.on("guildDelete", guild => {
     console.log("Left a guild: " + guild.name);
     guildMap.remove(guild.id);
 })
-
+/*
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+*/
 bot.login(process.env.token);
